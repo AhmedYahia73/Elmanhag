20c086caa7ca3d6defc8c10d98ba502e4af2c037
Ziad <wegostationdev@gmail.com>
2024-08-15 15:51:32 +0200

    End Subject Filter With Student

--- app/Http/Controllers/api/v1/student/setting/SubjectController.php
+++ app/Http/Controllers/api/v1/student/setting/SubjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api\v1\student\setting;
 
 use App\Http\Controllers\Controller;
 use App\Models\subject;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,13 +12,41 @@ class SubjectController extends Controller
 {
     public function __construct(
         private subject $subject
-    ){}
+    ) {}
     //This Controller About All Subject
 
-    public function show(int $education_id,){
-        
-        return response()->json(['success'=>'Successfully']);
-
-
+    public function show(Request $request)
+    {
+        $user_id = $request->user()->id;
+        $user =  $request
+            ->user()
+            ->where('id', $user_id)
+            ->with('category')->first();
+        $category = $user->category;
+        $category_id = $user->category->id;
+       $education_id = $request->education_id;
+        try {
+            if ($education_id) {
+                $subject = $this->subject
+                ->orderBy('name')
+                ->where('category_id', $category_id)
+                ->where('education_id', $education_id)->get();
+            } else {
+                $subject = $this->subject
+                ->where('category_id', $category_id)
+                ->orderBy('name')
+                ->get();
+            }
+            return response()->json([
+                'success' => 'Data Returned Successfully',
+                'category' => $category ,
+                'subject' => $subject ,
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'faield' => 'Data Not Found',
+                'data' => $e->getMessage(),
+            ]);
+        }
     }
 }
