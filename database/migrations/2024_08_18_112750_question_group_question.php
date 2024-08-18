7977fad52a7168d44bc60cb905d2d07edd6a4012
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-18 13:35:16 +0200

    make question group table and its pivot table

---
+++ database/migrations/2024_08_18_112750_question_group_question.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('question_group_question', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('question_group_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('question_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_group_question');
+    }
+};
