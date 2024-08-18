510807b0aa2aeb7b60e111a489be7b61bc40e535
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-18 15:42:21 +0200

    create delete homework function

--- app/Http/Controllers/api/v1/admin/homework/CreateHomeworkController.php
+++ app/Http/Controllers/api/v1/admin/homework/CreateHomeworkController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\api\v1\admin\homework;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\api\admin\homework\HomeworkRequest;
+
+use App\Models\homework;
 
 class CreateHomeworkController extends Controller
 {
-    //
+    public function __construct(private homework $homeworks){}
+    public function create(HomeworkRequest $request){
+
+    }
+    
+    public function modify(HomeworkRequest $request, $id){
+
+    }
+
+    public function delete($id){
+        $this->homeworks->where('id', $id)
+        ->delete();
+
+        return response()->json([
+            'success' => 'You delete data success'
+        ]);
+    }
 }
