7c552f56329a1079e0fc2a6e84b6d24d6156578b
Ziad <wegostationdev@gmail.com>
2024-08-18 14:51:03 +0200

    End Chapter Student APP

--- routes/student.php
+++ routes/student.php
@@ -1,6 +1,7 @@
 <?php
 namespace Student;
 
+use App\Http\Controllers\api\v1\student\chapter\ChapterController;
 use App\Http\Controllers\api\v1\student\setting\SettingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\StudentMiddleware;
@@ -25,12 +26,20 @@ Route::prefix('profile')->middleware(['auth:sanctum','IsStudent'])->group(functi
         Route::post('modify','update')->name('profile.view');
     });
 });
-Route::middleware('auth:sanctum')->prefix('setting')->group(function () {
-    Route::controller(SettingController::class)->withoutMiddleware('auth:sanctum')->group(function () {
-        Route::get('view','show')->name('setting.view');
-    }); // Guest Data
-    Route::controller(SubjectController::class)->group(function () {
-        Route::post('subject/view','show')->name('setting.view');
-        Route::get('subject/student','student_subject')->name('setting.view');
-    });
+Route::middleware(['auth:sanctum','IsStudent'])->group(function(){
+        Route::prefix('setting')->group(function () {
+            Route::controller(SettingController::class)->withoutMiddleware(['auth:sanctum','IsStudent'])->group(function () {
+                Route::get('view','show')->name('setting.view');
+            }); // Guest Data
+            Route::controller(SubjectController::class)->group(function () { // This All Subject For Student
+                Route::post('subject/view','show')->name('setting.view');
+                Route::get('subject/student','student_subject')->name('setting.view');
+            });
+        });
+        Route::prefix('mySubject')->group(function () { // This All Chapters For Student
+            Route::controller(ChapterController::class)->group(function () {
+                Route::post('chapter/view', 'show')->name('chapter_view');
+            });
+        });
+
 });
