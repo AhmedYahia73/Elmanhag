d27429e967f2ce03b22f2be27699b5f9be5b5e34
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-20 15:42:49 +0200

    make add function of promocode

--- app/Http/Controllers/api/v1/admin/promocode/CreatePromocodeController.php
+++ app/Http/Controllers/api/v1/admin/promocode/CreatePromocodeController.php
@@ -19,10 +19,27 @@ class CreatePromocodeController extends Controller
         'usage_type',
         'usage',
         'number_users',
+        'status',
     ];
 
     public function create(PromocodeRequest $request){
-        
+        // Keys
+        // title, code, status, value, precentage, usage_type, usage, number_users
+        // subjects[], bundles[]
+        $promocode_data = $request->only($this->promoCodeRequest);
+        $promo_code = $this->promo_code->create($promocode_data); // create promo code
+        // add subjects that have this promocode
+        foreach ($request->subjects as $item) {
+            $promo_code->subjects()->attach($item); // add subjects to pivot table
+        }
+        // add bundles that have this promocode
+        foreach ($request->bundles as $item) {
+            $promo_code->bundles()->attach($item); // add bundles to pivot table
+        }
+
+        return response()->json([
+            'success' => 'you add data success'
+        ]);
     }
     
     public function modify(PromocodeRequest $request, $id){
@@ -30,6 +47,12 @@ class CreatePromocodeController extends Controller
     }
     
     public function delete($id){
-        
+        $this->promo_code
+        ->where('id', $id)
+        ->delete();
+
+        return response()->json([
+            'success' => 'You delete data success'
+        ]);
     }
 }
