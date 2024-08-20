7cdc8887d9606c8a685ac5e5a1c85bf8b2a055d3
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-20 14:36:04 +0200

    make api of list as comment

--- app/Http/Controllers/api/v1/admin/promocode/PromocodeController.php
+++ app/Http/Controllers/api/v1/admin/promocode/PromocodeController.php
@@ -12,6 +12,7 @@ class PromocodeController extends Controller
     public function __construct(private PromoCode $promo_code){}
 
     public function show(){
+        // https://bdev.elmanhag.shop/admin/promoCode
         $promo_codes = $this->promo_code
         ->with(['subjects', 'bundles'])
         ->get();
