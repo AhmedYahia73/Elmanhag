1ca8c715e35d32aef04ba4e0bc127b503c4c8fcd
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-24 13:44:03 +0200

    make function of semester

--- app/Http/Controllers/api/v1/admin/settings/SettingsController.php
+++ app/Http/Controllers/api/v1/admin/settings/SettingsController.php
@@ -12,7 +12,27 @@ class SettingsController extends Controller
     public function __construct(private Setting $settings){}
 
     // Determine Semester
-    public function semester(Request $req){
+    public function semester(Request $request){
+        // key
+        // semester
+        $semester = $this->settings
+        ->where('name', 'semester')
+        ->first(); // Get setting of semester
 
+        if ( empty($semester) ) {
+            $semester->create([
+                'name' => 'semester',
+                'setting' => $request->semester,
+            ]); // if semester settings is not found insert new semester
+        }
+        else{
+            $semester->update([
+                'setting' => $request->semester,
+            ]);
+        } // if semester settings is found update semester
+
+        return response()->json([
+            'success' => 'You active semester success'
+        ]);
     }
 }
