7ba961cbf37c0ffa9e7ad13721152697c1f55a07
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-26 10:02:33 +0200

    make live table

---
+++ database/migrations/2024_08_26_072742_create_lives_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lives', function (Blueprint $table) {
+            $table->id();
+            $table->name('name');
+            $table->time('from');
+            $table->time('to');
+            $table->date('date');
+            $table->string('day');
+            $table->foreignId('teacher_id')->nullable()->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('subject_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->boolean('paid');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lives');
+    }
+};
