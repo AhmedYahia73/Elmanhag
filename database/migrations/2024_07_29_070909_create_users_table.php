ccd40ea74fc65ffa3a949a7d5de19d674bc20356
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-01 15:12:41 +0200

    make affilate structure

--- database/migrations/2024_07_29_070909_create_users_table.php
+++ database/migrations/2024_07_29_070909_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone');
             $table->string('image')->nullable()->default('default.png');
-            $table->enum('role',['supAdmin','admin','teacher','parent','student']);
+            $table->enum('role',['supAdmin','admin','teacher','parent','student', 'affilate']);
             $table->enum('gender',['mail','femail']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
