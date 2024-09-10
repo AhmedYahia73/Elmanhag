c57ca72b0f2e83580a33c0ade30cc871e1b65a40
Ziad <wegostationdev@gmail.com>
2024-09-10 10:35:55 +0200

    Edit In Affiliate Bonus

--- app/Http/Controllers/api/v1/affiliate/BonusController.php
+++ app/Http/Controllers/api/v1/affiliate/BonusController.php
@@ -3,26 +3,33 @@
 namespace App\Http\Controllers\api\v1\affiliate;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bonus;
 use App\Models\User;
+use Composer\Semver\Constraint\Bound;
 use Illuminate\Http\Request;
 
 class BonusController extends Controller
 {
     // This Is Controller About All Affiliate Bonus
 
-    public function __construct(private User $affilateBonus){}
+    public function __construct(private User $affilateBonus,private Bonus $bound){}
 
     public function get_bonus(Request $request){
         $user_id = $request->user()->id;
          $user = $request->user()->where('id',$user_id)->with('bonuses',function($query){
-            $query->orderByDesc('id')->first();
+            $query->orderByDesc('id');
          })->first();
-        $bonus = $user->bonuses->unique();
+        $bonus = $user->bonuses->unique()->first();
+        $target = $bonus->target;
+        $bundle_history = $user->affiliate_history->where('service_type','bundle')->count();
+        $total_bonus = $this->bound->get();
         // $targetBonus = $user->targetBonus->first();
 
         return response()->json([
             'success'=>'Bonus Returned Successfully',
             'affiliate_bonus'=>$bonus,
+            'affiliate_history'=>$bundle_history,
+            'bonus'=>$total_bonus ,
         ]);
     }
 }
