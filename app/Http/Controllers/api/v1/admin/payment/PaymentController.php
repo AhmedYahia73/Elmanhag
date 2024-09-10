3bd5d293e8801d7451f43390dbdd72638f10c0c2
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-10 13:58:49 +0200

    commit

--- app/Http/Controllers/api/v1/admin/payment/PaymentController.php
+++ app/Http/Controllers/api/v1/admin/payment/PaymentController.php
@@ -73,9 +73,6 @@ class PaymentController extends Controller
         // Determine Service That User Pay it
         if ( $payment->service == 'Subject' ) {
             $subjects = $payment->subject->pluck('id')->toArray(); // Get subjects as array
-            return response()->json([
-                'subjects' => $subjects
-            ]);
             if (count($subjects) > 0) {
                 $user->subjects()->sync($subjects); // Add subjects to student
             }
