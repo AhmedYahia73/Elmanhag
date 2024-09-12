b2dcedb128d996ba4069f12f11032388f429c71d
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-12 11:38:33 +0200

    update profile of parent

---
+++ app/Http/Controllers/api/v1/parent/profile/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\api\v1\parent\profile;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class ProfileController extends Controller
+{
+    public function __construct(private User $user){}
+    protected $parentRequest = [
+        'name',
+    ];
+
+    public function modify(Request $request){
+        $parentData = $request->only($this->parentRequest);
+        if (isset($request->password) && !empty($request->password)) {
+            $parentData['password'] = Hash::make($request->password);
+        }
+        $this->user->where('id', auth()->user()->id)
+        ->update($parentData);
+
+        return response()->json([
+            'success' => 'You update data success'
+        ]);
+    }
+}
--- routes/parent.php
+++ routes/parent.php
@@ -4,13 +4,20 @@ namespace Parent;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\v1\parent\childreen\ChildreenController;
+use App\Http\Controllers\api\v1\parent\profile\ProfileController;
 
 Route::middleware(['auth:sanctum', 'IsParent'])->group(function(){    
     // Start Childreen Module
     Route::prefix('childreen')->group(function() {
         Route::controller(ChildreenController::class)->group(function(){
             Route::get('/', 'show')->name('childreen.show');
-            Route::post('/profile/{id}', 'child_profile')->name('childreen.child_profile');
+            Route::put('/profile/{id}', 'child_profile')->name('childreen.child_profile');
+        });
+    });
+    // Start Profile Module
+    Route::prefix('profile')->group(function() {
+        Route::controller(ProfileController::class)->group(function(){
+            Route::put('/', 'modify')->name('profile.update');
         });
     });
 });
