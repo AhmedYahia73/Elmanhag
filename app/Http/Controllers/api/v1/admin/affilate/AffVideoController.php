3b381c9a3067c863760f1c374c6ac47f9445c1da
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-14 13:44:22 +0200

    make add affilate video

--- app/Http/Controllers/api/v1/admin/affilate/AffVideoController.php
+++ app/Http/Controllers/api/v1/admin/affilate/AffVideoController.php
@@ -4,19 +4,44 @@ namespace App\Http\Controllers\api\v1\admin\affilate;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\trait\image;
+use App\Http\Requests\api\admin\affilate\AffilateVideoRequest;
 
+use App\Models\AffilateGroupVideos;
 use App\Models\AffilateVideos;
 
 class AffVideoController extends Controller
 {
-    public function __construct(private AffilateVideos $videos){}
+    use image;
+    public function __construct(private AffilateVideos $videos, 
+    private AffilateGroupVideos $groups){}
+
+    protected $affilateVideoRequest = [
+        'title',
+        'affilate_group_video_id',
+    ];
 
     public function show(){
         $videos = $this->videos
         ->get();
+        $groups = $this->groups
+        ->get();
 
         return response()->json([
-            'videos' => $videos
+            'videos' => $videos,
+            'groups' => $groups,
+        ]);
+    }
+
+    public function add(AffilateVideoRequest $request){
+        $data = $request->only($this->affilateVideoRequest);
+        $video =  $this->upload($request,'video','admin/affilate/affilate_videos'); // Upload Video
+        $data['video'] = $video;
+        $this->videos
+        ->create($data);
+
+        return response()->json([
+            'success' => 'You add data success'
         ]);
     }
 }
---
+++ app/Http/Requests/api/admin/affilate/AffilateVideoRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\api\admin\affilate;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AffilateVideoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required'],
+            'affilate_group_video_id' => ['required', 'exists:affilate_group_videos,id'],
+            'video' => ['required', 'file']
+        ];
+    }
+
+    public function failedValidation(Validator $validator){
+       throw new HttpResponseException(response()->json([
+               'message'=>'validation error',
+               'errors'=>$validator->errors(),
+       ],400));
+   }
+}
