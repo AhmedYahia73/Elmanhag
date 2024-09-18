d7785cfab5a5e26e402ad67f5b961f3f3ddfc791
Ahmed <ahmedahmadahmid73@gmail.com>
2024-09-18 13:22:39 +0200

    add api of payment as comment

--- app/Http/Controllers/api/v1/student/paymentMethod/PaymentMethodController.php
+++ app/Http/Controllers/api/v1/student/paymentMethod/PaymentMethodController.php
@@ -10,8 +10,9 @@ use App\Models\PaymentMethod;
 class PaymentMethodController extends Controller
 {
     public function __construct(private PaymentMethod $payment_methods){}
-    
+
     public function view(){
+        // https://bdev.elmanhag.shop/student/paymentMethods
         $payment_methods = $this->payment_methods->get();
 
         return response()->json([
