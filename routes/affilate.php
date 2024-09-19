eefb79bc25b57fe5cf5657ecd762ddd339ad9670
Ziad <wegostationdev@gmail.com>
2024-09-19 11:30:05 +0200

    End Edit In Routes Affiliate and Student

--- routes/affilate.php
+++ routes/affilate.php
@@ -35,14 +35,19 @@ Route::middleware(['auth:sanctum', 'IsAffilate'])->group(function () {
     }); // Guest Data
 
 
-    Route::controller(ChapterController::class)->group(function () { // This All Chapters For Student
-        Route::prefix('subject')->group(function () {
+  
+  
+});
+Route::middleware(['auth:sanctum'])->group(function () {
+          Route::controller(ChapterController::class)->group(function () { // This All Chapters For Student
+            Route::prefix('subject')->group(function () {
             Route::post('chapter/view', 'chapters')->name('chapters');
         });
     });
-    Route::controller(LessonController::class)->group(function () { // This All Chapters For Student
+
+      Route::controller(LessonController::class)->group(function () { // This All Chapters For Student
         Route::prefix('chapter')->group(function () {
-            Route::post('lesson/view', 'show')->name('chapters');
+            Route::post('lesson/view', 'show')->name('chapters')->middleware('IsStudent');
         });
     });
 });
