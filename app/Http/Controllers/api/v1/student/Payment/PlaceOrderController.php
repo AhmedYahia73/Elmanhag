46ca3a6a7d81533e008737cc75f7e5b8ebe5ac5c
Ahmed <Wegostationdev@gmail.com>
2024-11-25 17:02:11 +0200

    commit

--- app/Http/Controllers/api/v1/student/Payment/PlaceOrderController.php
+++ app/Http/Controllers/api/v1/student/Payment/PlaceOrderController.php
@@ -128,7 +128,6 @@ class PlaceOrderController extends Controller
 
         }
         elseif ($newOrder['service'] == 'Recorded live') {
-            return 00;
             $livePayment = $newOrder->recorded_live()->attach($record_live_id); 
             $payment_oreder['order'] = $this->record_live
             ->where('id', $record_live_id)
