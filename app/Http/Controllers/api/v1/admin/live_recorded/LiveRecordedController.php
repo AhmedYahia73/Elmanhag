5670a7899ef01d1e596203f2c672c5dcb293efb1
Ahmed <Wegostationdev@gmail.com>
2024-11-26 09:46:23 +0200

    commit

--- app/Http/Controllers/api/v1/admin/live_recorded/LiveRecordedController.php
+++ app/Http/Controllers/api/v1/admin/live_recorded/LiveRecordedController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api\v1\admin\live_recorded;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\LiveRecorded;
 
@@ -21,6 +23,26 @@ class LiveRecordedController extends Controller
             'live_recorded' => $live_recorded
         ]);
     }
+
+    // public function included(Request $request, $id){                 
+    //     $validator = Validator::make($request->all(), [
+    //         'included' => 'required|boolean',
+    //     ]);
+    //     if ($validator->fails()) { // if Validate Make Error Return Message Error
+    //         return response()->json([
+    //             'error' => $validator->errors(),
+    //         ],400);
+    //     }
+    //     $this->live_recorded
+    //     ->where('id', $id)
+    //     ->update([
+    //         'included' => $request->included
+    //     ]);
+
+    //     return response()->json([
+    //         'success' => $request->included ? 'active' : 'not active'
+    //     ]);
+    // }
     
     public function live_item($id){
         // https://bdev.elmanhag.shop/admin/recordedLive/live_item/{id}
